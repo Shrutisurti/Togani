18b79a490cb6989561c65fdb6917c9e3f1a9bb5a
Shrutisurti <108811526+Shrutisurti@users.noreply.github.com>
2022-09-30 11:34:32 +0530

    position

--- src/components/ImageOverlay.js
+++ src/components/ImageOverlay.js
@@ -3,13 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/custom.css';
 import { Container, NavLink, Row } from 'react-bootstrap';
 import Group from '../images/Group.png';
-import Group35793 from '../images/Group35793.png';
 
 
 function ImageOverlay() {
   return (
     <div>
-     <Container>
+    <Container style={{position:'relative'}}>
          <Row>
             <div className='col-lg-6'>
               <img src={Group} alt='Sweet' />
@@ -17,13 +16,7 @@ function ImageOverlay() {
                <NavLink className='btnStyle' to='/'>Shop Now</NavLink>
                </div>
                </div>
-            <div className='col-lg-6'>
-            <img src={Group35793} alt='Sweet' />
-            <div className='btn-position1'>
-               <NavLink to='/' className='btnStyle'
-               >Shop Now</NavLink>
-               </div>
-               </div>
+         
           </Row>
      </Container>
     </div>
